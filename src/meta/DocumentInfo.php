7d0bdf7fbc507b6d1e68326d293313919106e2b9
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-05-13 22:35:03 +0200

    Document meta structure created.

---
+++ src/meta/DocumentInfo.php
@@ -0,0 +1,14 @@
+<?php namespace insma\otuspdf\meta;
+
+class DocumentInfo extends \insma\otuspdf\base\BaseObject
+{
+    public $title;
+    public $author;
+    public $subject;
+    public $keywords;
+    public $creator;
+    public $producer;
+    public $creationDate;
+    public $modificationDate;
+    public $trapped;
+}
