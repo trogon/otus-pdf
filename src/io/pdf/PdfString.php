6fa94cdd0b7db627a7d219e1861b73260eb2bb41
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-05-13 22:37:51 +0200

    PDF String object implemented to store ASCII strings.

--- src/io/pdf/PdfString.php
+++ src/io/pdf/PdfString.php
@@ -2,5 +2,39 @@
 
 class PdfString extends \insma\otuspdf\base\BaseObject
 {
+    public const TYPE_LITERAL = 0;
+    public const TYPE_HEX = 1;
 
+    private $type = self::TYPE_LITERAL;
+    private $value;
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    public function toString()
+    {
+        if ($this->type === self::TYPE_HEX) {
+            $content = '<' . $this->value . '>';
+        } else {
+            $content = '(' . $this->value . ')';
+        }
+        return $content;
+    }
 }
