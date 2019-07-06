f05222e29a5247ed789690e72c7825f9daf294e7
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-07-07 00:19:24 +0200

    Page Position Info API introduced to map 2D location.

---
+++ src/meta/PositionInfo.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Otus PDF - PDF document generation library
+ * Copyright(C) 2019 Maciej Klemarczyk
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+namespace trogon\otuspdf\meta;
+
+class PositionInfo extends \trogon\otuspdf\base\BaseObject
+{
+    private $x;
+    private $y;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    public function getY()
+    {
+        return $this->y;
+    }
+}
