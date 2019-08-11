bcb5be6b7043511b892a3bd6289193a25c2ce09c
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-08-11 20:06:30 +0200

    First implementation of table render. Render method need to be revised.

--- src/io/BlockRender.php
+++ src/io/BlockRender.php
@@ -24,6 +24,7 @@ use trogon\otuspdf\io\InlineRender;
 use trogon\otuspdf\io\PdfContentBuilder;
 use trogon\otuspdf\PageBreak;
 use trogon\otuspdf\Paragraph;
+use trogon\otuspdf\Table;
 use trogon\otuspdf\TextBlock;
 
 class BlockRender extends \trogon\otuspdf\base\DependencyObject
@@ -74,6 +75,11 @@ class BlockRender extends \trogon\otuspdf\base\DependencyObject
         return $pageContentBox;
     }
 
+    public function computeTableBox($tableInfo, $pageContentBox)
+    {
+        return $pageContentBox;
+    }
+
     public static function updateRemainingBox($box, $width, $height)
     {
         return new RectInfo(
@@ -111,6 +117,15 @@ class BlockRender extends \trogon\otuspdf\base\DependencyObject
                 $content = '';
                 $remainingBox = $pageContentBox;
                 $inlineRender->resetRemainingBox();
+            } elseif ($block instanceof Table) {
+                $tableRender = new TableRender(
+                    $this->contentBuilder,
+                    $this->fontRender,
+                    $pageContentBox
+                );
+                $blockBox = $this->computeTableBox($block->info, $remainingBox);
+                $content .= $tableRender->render($block, $blockBox);
+                $remainingBox = $tableRender->remainingBox;
             }
         }
         if (!empty($content)) {
---
+++ src/io/TableRender.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * Otus PDF - PDF document generation library
+ * Copyright(C) 2019 Maciej Klemarczyk
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+namespace trogon\otuspdf\io;
+
+use trogon\otuspdf\base\InvalidCallException;
+use trogon\otuspdf\meta\InlineInfo;
+use trogon\otuspdf\meta\PositionInfo;
+use trogon\otuspdf\meta\RectInfo;
+use trogon\otuspdf\io\InlineRender;
+use trogon\otuspdf\io\FontRender;
+use trogon\otuspdf\io\PdfContentBuilder;
+use trogon\otuspdf\PageBreak;
+use trogon\otuspdf\Paragraph;
+use trogon\otuspdf\Table;
+use trogon\otuspdf\TextBlock;
+
+class TableRender extends \trogon\otuspdf\base\DependencyObject
+{
+    const TABLE_MARGIN = 72 / 8;
+
+    private $contentBuilder;
+    private $defaultInlineInfo;
+    private $fontRender;
+    private $pageContentBox;
+    private $remainingBox;
+    private $textRender;
+
+    public function __construct(
+        PdfContentBuilder $contentBuilder,
+        FontRender $fontRender,
+        RectInfo $pageContentBox
+    )
+    {
+        $this->contentBuilder = $contentBuilder;
+        $this->fontRender = $fontRender;
+        $this->pageContentBox = $pageContentBox;
+        parent::__construct();
+    }
+
+    public function init()
+    {
+        parent::init();
+        $this->remainingBox = $this->pageContentBox;
+        $this->textRender = new TextRender();
+        $this->defaultInlineInfo = new InlineInfo([
+            'fontFamily' => 'Times-Roman',
+            'fontSize' => 14
+        ]);
+    }
+
+    public function getRemainingBox()
+    {
+        return $this->remainingBox;
+    }
+
+    public function render(Table $tableBlock, RectInfo $tableBox)
+    {
+        $inlineRender = new InlineRender(
+            $this->contentBuilder,
+            $this->fontRender,
+            $tableBox
+        );
+
+        $cb = $this->contentBuilder;
+        $columnBoxes = $this->computeColumnBoxes($tableBlock->columns, $tableBox);
+        $columnCount = count($columnBoxes);
+
+        $content = '';
+        $content .= $cb->setStrokeColorRgb(24, 16, 182);
+
+        $remainingBox = $tableBox;
+        foreach ($tableBlock->rowGroups as $grn => $rowGroup) {
+            foreach ($rowGroup->rows as $rn => $row) {
+                $cellBoxes = [];
+                foreach ($row->cells as $cn => $cell) {
+                    $cellBox = $this->computeCellBox($cell->info, $columnBoxes[$cn]);
+                    $cellBoxes[$cn] = $cellBox;
+                    $cellRemaining = $cellBox;
+                    foreach ($cell->blocks as $bn => $block) {
+                        if ($block instanceof Paragraph) {
+                            $content .= $inlineRender->renderInlines($block->inlines, $cellRemaining, $block->info);
+                            $cellRemaining = $inlineRender->remainingBox;
+                        }
+                    }
+                    $cellRemaining = BlockRender::updateRemainingBox($cellRemaining, 0, self::TABLE_MARGIN);
+                    $columnBoxes[$cn] = $cellRemaining;
+                }
+                // Allign columns space
+                $minY = $columnBoxes[0]->y;
+                foreach ($columnBoxes as $columnBox) {
+                    $minY = min($minY, $columnBox->y);
+                }
+                foreach ($columnBoxes as $cn => $columnBox) {
+                    if ($columnBox->y > $minY) {
+                        $columnBoxes[$cn] = BlockRender::updateRemainingBox(
+                            $columnBox, 0, $columnBox->y - $minY
+                        );
+                    }
+                    if (array_key_exists($cn, $cellBoxes)) {
+                        $cellBox = $cellBoxes[$cn];
+                        $cellBorderBox = new RectInfo(
+                            $cellBox->x,
+                            $cellBox->y,
+                            $cellBox->width,
+                            $cellBox->top - $minY,
+                            $cellBox->orientation
+                        );
+                        $content .= $cb->drawRectangle($cellBorderBox->x, $cellBorderBox->y, $cellBorderBox->width, -$cellBorderBox->height);
+                        $content .= $cb->endPath();
+                    }
+                }
+            }
+        }
+        $this->remainingBox = new RectInfo(
+            $columnBoxes[0]->x,
+            $columnBoxes[0]->y,
+            $remainingBox->width,
+            $columnBoxes[0]->height,
+            $remainingBox->orientation
+        );
+
+        return $content;
+    }
+
+    public static function computeColumnBoxes($columns, $tableBox)
+    {
+        $columnCount = count($columns);
+        $width = floatval($tableBox->width) / $columnCount;
+        $x = floatval($tableBox->x);
+
+        $boxes = [];
+        foreach ($columns as $cn => $column) {
+            $boxes[] = new RectInfo(
+                $x,
+                $tableBox->y - self::TABLE_MARGIN,
+                $width,
+                $tableBox->height - self::TABLE_MARGIN,
+                $tableBox->orientation
+            );
+            $x += $width;
+        }
+
+        return $boxes;
+    }
+
+    public static function computeCellBox($info, $box)
+    {
+        return $box;
+    }
+}
