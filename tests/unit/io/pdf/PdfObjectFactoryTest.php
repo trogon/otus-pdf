eb160f52340ba62bf9988eed4a8fe562833129d0
Maciej Klemarczyk <m.klemarczyk+git@live.com>
2019-08-13 14:29:27 +0200

    PdfObjectFactory unit tests

---
+++ tests/unit/io/pdf/PdfObjectFactoryTest.php
@@ -0,0 +1,59 @@
+<?php
+namespace trogon\otuspdf\test\unit\io\pdf;
+
+use PHPUnit\Framework\TestCase;
+
+use trogon\otuspdf\io\pdf\PdfObjectFactory;
+use trogon\otuspdf\base\InvalidCallException;
+
+final class PdfObjectFactoryTest extends TestCase
+{
+    private $pdfObjectClass = 'trogon\otuspdf\io\pdf\PdfObject';
+    private $pdfObjectFactoryClass = 'trogon\otuspdf\io\pdf\PdfObjectFactory';
+    private $invalidCallExceptionClass = 'trogon\otuspdf\base\InvalidCallException';
+
+    public function testCanBeCreatedFromEmptyConfig()
+    {
+        $this->assertInstanceOf(
+            $this->pdfObjectFactoryClass,
+            new PdfObjectFactory()
+        );
+    }
+
+    public function testCreateReturnsPdfObject()
+    {
+        $pdfObjectFactory = new PdfObjectFactory();
+
+        $this->assertInstanceOf(
+            $this->pdfObjectClass,
+            $pdfObjectFactory->create()
+        );
+    }
+
+    public function testCreateReturnsFirstObjectsIdEqualsOne()
+    {
+        $pdfObjectFactory = new PdfObjectFactory();
+
+        $firstObject = $pdfObjectFactory->create();
+
+        $this->assertEquals(
+            1,
+            $firstObject->id
+        );
+    }
+
+    public function testCreateReturnsObjectsWithNextId()
+    {
+        $pdfObjectFactory = new PdfObjectFactory();
+        $pdfObjectFactory->create();
+
+        for ($i = 2; $i < 5; $i++) {
+            $nextObject = $pdfObjectFactory->create();
+
+            $this->assertEquals(
+                $i,
+                $nextObject->id
+            );
+        }
+    }
+}
